0be1eda3e1604707c7e03b0da10f4d93e416eb93
koolung <taeyun.koolung@gmail.com>
2026-03-06 16:14:19 -0400

    cors origin to vercel stg env 2

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const corsOrigins = [
     'http://localhost:3000',
     'https://appointment-app-frontend-3h9qc8iyl-koolungs-projects.vercel.app',
+    'https://appointment-app-frontend.vercel.app',
   ];
   if (process.env.CORS_ORIGIN) {
     corsOrigins.push(process.env.CORS_ORIGIN);
